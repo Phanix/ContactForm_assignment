ea487c73b0f5a00d43bc0dcd1b3b2430bd25510c
fabio henrique da silva <hantarodev@gmailcom>
2019-06-24 15:03:02 -0300

    Added final String

--- app/src/main/java/hantaro/com/contactform/DetailContact.java
+++ app/src/main/java/hantaro/com/contactform/DetailContact.java
@@ -1,13 +1,75 @@
 package hantaro.com.contactform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailContact extends AppCompatActivity {
-
+    public static final String NAME_KEY = "name";
+    public static final String EMAIL_KEY = "email";
+    public static final String BIRTHDATE_KEY = "birthdate";
+    public static final String PHONE_KEY = "phone";
+    public static final String DESCRIPTION_KEY = "description";
+    String name;
+    String email;
+    String birthDate;
+    String description;
+    String phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_contact);
+
+        Bundle bundle = getIntent().getExtras();
+         name = bundle.getString(NAME_KEY);
+         email = bundle.getString(EMAIL_KEY);
+         birthDate = bundle.getString(BIRTHDATE_KEY);
+         description = bundle.getString(DESCRIPTION_KEY);
+         phone = bundle.getString(PHONE_KEY);
+
+        TextView tvName = findViewById(R.id.tv_name);
+        TextView tvEmail = findViewById(R.id.tv_email);
+        TextView tvBirthDate = findViewById(R.id.tv_birth_date);
+        TextView tvDescription = findViewById(R.id.tv_description);
+        TextView tvPhone = findViewById(R.id.tv_phone_number);
+        tvPhone.append(phone);
+        tvDescription.append(description);
+        tvBirthDate.append(birthDate);
+        tvEmail.append(email);
+        tvName.append(name);
+
+        Button editButton = findViewById(R.id.edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewAcitivity();
+            }
+        });
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            startNewAcitivity();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void startNewAcitivity(){
+        Intent intent = new Intent(DetailContact.this, MainActivity.class);
+        intent.putExtra(NAME_KEY, name);
+        intent.putExtra(EMAIL_KEY, email);
+        intent.putExtra(BIRTHDATE_KEY, birthDate);
+        intent.putExtra(DESCRIPTION_KEY, description);
+        intent.putExtra(PHONE_KEY, phone);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/hantaro/com/contactform/MainActivity.java
+++ app/src/main/java/hantaro/com/contactform/MainActivity.java
@@ -1,27 +1,60 @@
 package hantaro.com.contactform;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
     TextView mTvBirthDate;
     int mMonth = 1;
     int mDay = 1;
     int mYear = 1970;
+
+    EditText mName;
+    EditText mEmail;
+    EditText mPhoneNumber;
+    EditText mContactDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTvBirthDate = findViewById(R.id.birthDateText);
         ImageView imageView = findViewById(R.id.birthDate);
+
+        mName = findViewById(R.id.et_name);
+        mEmail = findViewById(R.id.et_email);
+        mContactDescription = findViewById(R.id.et_description);
+        mPhoneNumber = findViewById(R.id.et_phone_number);
+
+        if(getIntent().hasExtra(DetailContact.NAME_KEY)){
+            Bundle bundle = getIntent().getExtras();
+            mEmail.setText(bundle.getString(DetailContact.EMAIL_KEY));
+            mName.setText(bundle.getString(DetailContact.NAME_KEY));
+            mPhoneNumber.setText(bundle.getString(DetailContact.PHONE_KEY));
+            mContactDescription.setText(bundle.getString(DetailContact.DESCRIPTION_KEY));
+            String birthDate =  bundle.getString(DetailContact.BIRTHDATE_KEY);
+            mTvBirthDate.setText(birthDate);
+            String [] birthDateArray = birthDate.split("/");
+            mMonth = Integer.parseInt(birthDateArray[0]);
+            mDay   = Integer.parseInt(birthDateArray[1]);
+            mYear = Integer.parseInt(birthDateArray[2]);
+        }
+
+
 
 
     imageView.setOnClickListener(new View.OnClickListener() {
@@ -43,10 +76,30 @@ public class MainActivity extends AppCompatActivity {
 
         }
     });
+
+        Button button = findViewById(R.id.bt_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewAcitivity();
+            }
+        });
     }
 
+    public void startNewAcitivity(){
+        Intent intent = new Intent(MainActivity.this, DetailContact.class);
+        intent.putExtra(DetailContact.NAME_KEY, mName.getText().toString());
+        intent.putExtra(DetailContact.EMAIL_KEY, mEmail.getText().toString());
+        intent.putExtra(DetailContact.BIRTHDATE_KEY, mTvBirthDate.getText().toString());
+        intent.putExtra(DetailContact.DESCRIPTION_KEY, mContactDescription.getText().toString());
+        intent.putExtra(DetailContact.PHONE_KEY, mPhoneNumber.getText().toString());
+        startActivity(intent);
+        finish();
+    }
 
     public String formatDateText(int month, int day, int year){
         return month + "/" + day + "/" + year;
     }
+
+
 }
